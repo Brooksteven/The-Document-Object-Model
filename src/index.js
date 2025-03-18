aa598bab0cb929a1372323099aa1006a0b498728
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-03-18 15:55:03 -0400

    cached my elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 //All Resources will be in the README.md
 
+//  Cached elements //
+// Requirement: Cache at least one element using getElementById (5%)
+const form = document.getElementById("registration");
+
+// Requirement: Cache at least one element using querySelector (5%)
+const errorDisplay = document.querySelector("#errorDisplay");
+
+// Cache login elements 
+const loginForm = document.getElementById("login");
+const loginUsername = loginForm.elements["username"];
+const loginPassword = loginForm.elements["password"];
+const rememberMe = loginForm.elements["persist"];
 
